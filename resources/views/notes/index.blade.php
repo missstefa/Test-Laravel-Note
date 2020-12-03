b17ea3999c29ab2c379bc2fcd3b52bbd6c6c40c0
Stefa <miss.stefa1517@gmail.com>
2020-12-03 21:14:32 +0300

    .

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
     <table class="table">
-        <caption style="caption-side:top; text-align:center;  font-size: large; font-weight: bold">Список заметок</caption>
+        <caption style="caption-side:top; text-align:center;  font-size: large; font-weight: bold">List of notes</caption>
         <thead class="thead-dark">
         <tr>
             <th scope="col">#</th>
             <th scope="col">Title</th>
             <th scope="col">Important</th>
+            <th scope="col">Created date</th>
             <th scope="col">Actions</th>
         </tr>
         </thead>
@@ -17,6 +18,7 @@
                 <td>{{ $note->id }}</td>
                 <td>{{ $note->title }}</td>
                 <td>{{ $note->is_important }}</td>
+                <td>{{ $note->getFormatDateForIndex() }}</td>
                 <td>
                     <form method="GET" action="{{ route('notes_show',['note' => $note->id]) }}">
                         @csrf
